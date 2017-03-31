326336ef46cc0a6e7f6e616b75e289a3a6474f06
Antonio Ricardo <antonio.eschola@gmail.com>
2017-03-31 16:11:02 -0400

    Primeiro commit do arquivo inicial do game.

---
+++ maior_ou_menor.rb
@@ -0,0 +1,16 @@
+puts "Bem-vindo ao jogo da adivinhação."
+puts 
+puts
+puts
+puts "Qual é o seu nome?"
+name = gets
+puts
+puts
+puts
+puts "Começaremos o jogo para você, " + name
+puts
+puts
+puts "Escolhendo um número secreto entre 0 e 200..."
+number_secret = 178
+puts "Escolhido.. Que tal adivinhar hoje nosso número secreto.?"
+
