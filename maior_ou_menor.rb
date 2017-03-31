581c9176e1d1b5bca1110ba39ec9fc356eed5523
Antonio Ricardo <antonio.eschola@gmail.com>
2017-03-31 16:20:09 -0400

    Refatorando código.

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,26 +1,17 @@
 puts "Bem-vindo ao jogo da adivinhação."
-puts 
-puts
-puts
+puts "\n\n"
 puts "Qual é o seu nome?"
 name = gets
-puts
-puts
-puts
+puts "\n\n"
 puts "Começaremos o jogo para você, " + name
-puts
-puts
+puts "\n\n"
 puts "Escolhendo um número secreto entre 0 e 200..."
 number_secret = 178
 puts "Escolhido.. Que tal adivinhar hoje nosso número secreto.?"
-
-
-puts
-puts
-puts
-puts
-puts
+puts "\n\n\n\n"
 puts "Tentativa 1"
 puts "Escolha um número: "
 kick = gets
+# convertendo a string para inteiro
+kick = kick.to_i
 puts  "Será que você acertou? Você escolher o número: " + kick
