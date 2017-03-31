2e47bc9d805f8132c19676985fd650baedf481b4
Antonio Ricardo <antonio.eschola@gmail.com>
2017-03-31 19:50:16 -0400

    Melhorando e refatorando código do Game

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -8,22 +8,39 @@ puts "\n\n"
 puts "Escolhendo um número secreto entre 0 e 200..."
 number_secret = 178
 puts "Escolhido.. Que tal adivinhar hoje nosso número secreto.?"
+
+limit = 10
+
+for attempt in 1..limit
+
 puts "\n\n\n\n"
-puts "Tentativa 1"
+puts "Tentativa " + attempt.to_s + " de " + limit.to_s
 puts "Escolha um número: "
 kick = gets
 # convertendo a string para inteiro
 kick = kick.to_i
-puts  "Será que você acertou? Você escolheu o número: " + kick
+puts  "Será que você acertou? Você escolheu o número: " + kick.to_s
 
 # controle de condição do chute 
 
 hit =  kick == number_secret
 
 if hit
-	puts "Parabéns, você acertou :)"
+        puts "Parabéns, você acertou :)"
+	break
 else
-	puts "Humm... você errou :("
+        puts "Humm... você errou :("
+larger = number_secret > kick
+        if larger
+        puts "O número secreto é maior."
+        else
+        puts "O número secreto é menor."
+        end
 end
+
+
+
+end
+
 
 
